b257dac995a9b12769140a72c1d2d7bc22018124
Rushil Choudhary <choudharyrushil118@gmail.com>
2025-09-08 14:10:09 +0530

    updated facade

--- design-pattern-assignments/facade-report-bundle/src/com/example/report/ReportBundleFacade.java
+++ design-pattern-assignments/facade-report-bundle/src/com/example/report/ReportBundleFacade.java
@@ -6,11 +6,12 @@ public class ReportBundleFacade {
     static AuditLog log = new AuditLog();
     static JsonWriter writer = new JsonWriter();
     static Zipper zipper = new Zipper();
+    
 
     public static Path export(java.util.Map<String,Object> data, java.nio.file.Path outDir, String baseName){
-        Path json = writer.write(data, Path.of("out"), baseName);
-        Path zip = zipper.zip(json, Path.of("out", baseName));
-        log.log("exported" + zip);
+        Path json = writer.write(data, outDir, baseName);
+        Path zip = zipper.zip(json, outDir.resolve(baseName));
+        log.log("exported " + zip);
         return zip;
     }
 
